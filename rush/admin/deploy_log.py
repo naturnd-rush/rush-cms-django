5f2d55515fa8c4891cf9abbe9f1f63f3aaa1b871
Sam <dodobird181@gmail.com>
2025-05-03 19:24:49 -0400

    make deploy logs immutable from the admin site

--- rush/admin/deploy_log.py
+++ rush/admin/deploy_log.py
@@ -11,3 +11,12 @@ class DeployLogAdmin(admin.ModelAdmin):
 
     def log_contents(self, obj: DeployLog) -> str:
         return obj.get_log_contents()
+
+    def has_change_permission(self, request, obj=...):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=...):
+        return False
