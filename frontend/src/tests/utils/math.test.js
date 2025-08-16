cbee2e0752fa0b730fafeb5ed878dcf8aa47b092
Sam <dodobird181@gmail.com>
2025-08-16 11:59:49 -0700

    add vite test

--- frontend/src/tests/utils/math.test.js
+++ frontend/src/tests/utils/math.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { interpolateNumbers } from '../../utils/math'
+import { interpolateNumbers, coerceNumbersDeep } from '../../utils/math'
 
 
 test('interpolate numbers should split the difference evenly', () => {
@@ -20,4 +20,10 @@ test('interpolate numbers should return unedfined when both inputs are null or u
   expect(interpolateNumbers(undefined, undefined)).toBe(undefined);
   expect(interpolateNumbers(null, undefined)).toBe(undefined);
   expect(interpolateNumbers(undefined, null)).toBe(undefined);
+});
+
+test('coerce numbers deep should coerce strings, lists, and booleans', () => {
+  expect(coerceNumbersDeep({"foo": "5.1"})).toStrictEqual({"foo": 5.1});
+  expect(coerceNumbersDeep({"foo": [true, "2", "3"]})).toStrictEqual({"foo": [1, 2, 3]});
+  expect(coerceNumbersDeep({"foo": true, "bar": false})).toStrictEqual({"foo": 1, "bar": 0});
 });
