d5a51bd5b3ac73b7383653e44cc8180013391173
Samuel Morris <53889741+dodobird181@users.noreply.github.com>
2025-09-30 23:10:19 +0000

    Revert "Use absolute paths for POETRY and PYTHON in Deploy Script"

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,10 @@
 set -euo pipefail
 
 cd /srv/rush-cms-django
-PYTHON="/srv/rush-cms-django/.venv/bin/python" # Want project-specific Python virtual environment.
-POETRY="/home/deploy/.local/bin/poetry" # BUT must use deployer's Poetry installation (since it only has permission to run that version of Poetry).
-
 git pull origin main
-
-"$POETRY" lock
-"$POETRY" install
-"$PYTHON" manage.py migrate
-"$PYTHON" manage.py collectstatic --noinput
+/home/deploy/.local/bin/poetry lock
+/home/deploy/.local/bin/poetry install
+#poetry run python manage.py makemigrations <-- migrations should always be added by git
+/home/deploy/.local/bin/poetry run python manage.py migrate
+/home/deploy/.local/bin/poetry run python manage.py collectstatic --noinput
 sudo systemctl restart gunicorn.service
