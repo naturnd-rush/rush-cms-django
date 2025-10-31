f04887e29620381b888b8e37703c21e3c31a6045
Sam <dodobird181@gmail.com>
2025-10-30 22:40:14 -0400

    add initiative tag colors to graphql api

--- rush/graphql.py
+++ rush/graphql.py
@@ -161,7 +161,7 @@ class QuestionTabType(DjangoObjectType):
 class InitiativeTagType(DjangoObjectType):
     class Meta:
         model = models.InitiativeTag
-        fields = ["id", "name"]
+        fields = ["id", "name", "text_color", "background_color"]
 
 
 class InitiativeType(DjangoObjectType):
