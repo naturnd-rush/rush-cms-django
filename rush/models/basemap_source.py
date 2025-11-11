f7b5a73bb40e267534c01629769e7bd1bea8dbe2
Sam <dodobird181@gmail.com>
2025-11-11 12:07:58 -0500

    add comment

--- rush/models/basemap_source.py
+++ rush/models/basemap_source.py
@@ -1,14 +1,18 @@
-from django.db import models
-from django.core.validators import URLValidator
 import uuid
+
+from django.core.validators import URLValidator
+from django.db import models
 
 
 class BasemapSource(models.Model):
+    """
+    The source location of a "basemap," which is a tile-set used to style maps.
+    """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False)
-    tile_url = models.CharField(max_length=2000,null=False,validators=[URLValidator(schemes=["https"])])
+    tile_url = models.CharField(max_length=2000, null=False, validators=[URLValidator(schemes=["https"])])
     max_zoom = models.PositiveIntegerField()
     attribution = models.TextField()
-
 
     def __str__(self):
         return self.tile_url
