52ae5b8fc2ed11b6e834e690a01b8a7426d25e13
Sam <dodobird181@gmail.com>
2025-11-14 17:32:56 -0500

    log changeform and changelist views in LogEntry

--- rush/apps.py
+++ rush/apps.py
@@ -4,3 +4,52 @@ from django.apps import AppConfig
 class RushConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "rush"
+
+    def ready(self):
+        self.patch_admin_site_to_emit_changelist_view_signals()
+        self.patch_admin_site_to_emit_changeform_view_signals()
+
+    def patch_admin_site_to_emit_changeform_view_signals(self):
+        """
+        Patch all admin models to emit a signal when a model's changeform is viewed.
+        """
+
+        from django.contrib.admin import ModelAdmin
+
+        from rush.models.signals import admin_changeform_viewed
+
+        original_changeform_view = ModelAdmin.changeform_view
+
+        def changeform_wrapper(self, request, object_id=None, *args, **kwargs):
+            admin_changeform_viewed.send(
+                sender=self.__class__,
+                request=request,
+                model=self.model,
+                object_id=object_id,
+                modeladmin=self,
+            )
+            return original_changeform_view(self, request, object_id, *args, **kwargs)
+
+        ModelAdmin.changeform_view = changeform_wrapper
+
+    def patch_admin_site_to_emit_changelist_view_signals(self):
+        """
+        Patch all admin models to emit a signal when a model's changelist is viewed.
+        """
+
+        from django.contrib.admin import ModelAdmin
+
+        from rush.models.signals import admin_changelist_viewed
+
+        original_changelist_view = ModelAdmin.changelist_view
+
+        def changelist_wrapper(self, request, *args, **kwargs):
+            admin_changelist_viewed.send(
+                sender=self.__class__,
+                request=request,
+                model=self.model,
+                modeladmin=self,
+            )
+            return original_changelist_view(self, request, *args, **kwargs)
+
+        ModelAdmin.changelist_view = changelist_wrapper
---
+++ rush/models/signals/viewing.py
@@ -0,0 +1,53 @@
+from django.contrib.admin.models import ADDITION, LogEntry
+from django.db.models import Model
+from django.dispatch import Signal, receiver
+
+from rush.models.signals.utils import user_content_type
+from rush.utils import get_client_ip
+
+# Fired when *any* admin changelist is viewed
+admin_changelist_viewed = Signal()
+
+# Fired when *any* admin changeform is viewed
+admin_changeform_viewed = Signal()
+
+
+@receiver(admin_changelist_viewed)
+def log_changelist(sender, request, model, modeladmin, **kwargs):
+    user = getattr(request, "user", None)
+    if user is not None:
+        LogEntry.objects.log_action(
+            user_id=user.pk,
+            content_type_id=user_content_type().pk,
+            object_id=user.pk,
+            object_repr=str(user)[:200],
+            action_flag=ADDITION,
+            change_message=f"{user} viewed the list of {model._meta.verbose_name_plural}.",
+        )
+    else:
+        # LOG TODO Should log a warning here...
+        ...
+
+
+@receiver(admin_changeform_viewed)
+def log_changeform(sender, request, model, object_id, modeladmin, **kwargs):
+    user = getattr(request, "user", None)
+    if user is not None:
+        try:
+            obj = model.objects.get(id=object_id)
+            obj_desc = f"<{model.__name__}: {str(obj)}>"
+            LogEntry.objects.log_action(
+                user_id=user.pk,
+                content_type_id=user_content_type().pk,
+                object_id=user.pk,
+                object_repr=str(user)[:200],
+                action_flag=ADDITION,
+                change_message=f"{user} viewed {obj_desc}.",
+            )
+        except Exception as e:
+            # LOG TODO: should log a warning here...
+            ...
+    else:
+        # LOG TODO: Should log a warning here...
+        ...
+    print(f"[ADMIN] changeform for {model.__name__}({object_id}) viewed by {request.user}")
