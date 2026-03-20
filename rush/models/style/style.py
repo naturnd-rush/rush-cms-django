343f4b1c9678736b83a804120f072193dd12cb92
Sam <dodobird181@gmail.com>
2026-03-20 18:50:28 -0400

    migrate style to include circle fields

--- rush/models/style/style.py
+++ rush/models/style/style.py
@@ -48,6 +48,10 @@ class FillRule(models.TextChoices):
 class Style(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False)
     name = models.CharField(max_length=255)
+
+    ##########
+    # Stroke #
+    ##########
     draw_stroke = models.BooleanField(
         help_text="Check this box when you want to draw the line. Unchecking this box, for example, "
         + "will remove the borders from a polygon."
@@ -95,6 +99,10 @@ class Style(models.Model):
         blank=True,
         help_text="Offset where the dash pattern starts along the outline path.",
     )
+
+    ########
+    # Fill #
+    ########
     draw_fill = models.BooleanField(
         help_text="Check this box if you want to fill the polygon.",
         default=True,
@@ -118,6 +126,10 @@ class Style(models.Model):
         null=True,
         blank=True,
     )
+
+    ##########
+    # Marker #
+    ##########
     draw_marker = models.BooleanField(
         help_text="Check this box if you want to draw the marker icon on each point this style is applied to.",
         default=True,
@@ -156,6 +168,67 @@ class Style(models.Model):
         default=Decimal(0.8),
         null=True,
         blank=True,
+    )
+
+    ##########
+    # Circle #
+    ##########
+    draw_circle = models.BooleanField(
+        help_text="Check this box if you want to fill a polygon area with circles instead of a solid color. IMPORTANT: the circles "
+        + "are drawn using 'georeferenced units,' meaning that their size is determined by meters on the map, as opposed to pixels on the screen.",
+        default=False,
+    )
+    circle_radius = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal(5),
+        verbose_name="Radius",
+    )
+    circle_color = ColorField(
+        default="#032697",
+        verbose_name="Color",
+    )
+    circle_weight = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=Decimal(1),
+        verbose_name="Thickness",
+    )
+    circle_opacity = models.DecimalField(
+        max_digits=5,
+        decimal_places=3,
+        default=Decimal(1.0),
+        verbose_name="Transparency",
+    )
+    circle_line_cap = models.CharField(
+        # See Mozilla docs: https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Attribute/stroke-linecap#usage_notes.
+        max_length=32,
+        choices=LineCap.choices,
+        default=LineCap.ROUND,
+        help_text="The shape of the end of a stroke. Options: butt, round, or square.",
+    )
+    circle_line_join = models.CharField(
+        # See Mozilla docs: https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Attribute/stroke-linejoin#usage_context.
+        max_length=32,
+        choices=LineJoin.choices,
+        default=LineJoin.ROUND,
+        help_text="The shape used to join two lines. Options: arcs, bevel, miter, miter clip, or round.",
+    )
+    circle_dash_array = models.CharField(
+        # See Mozilla docs: https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Attribute/stroke-dasharray#example.
+        max_length=32,
+        validators=[validate_only_integers_and_whitespace],
+        null=True,
+        blank=True,
+        help_text="The pattern of dashes and gaps for the outline, e.g., '5 5' for 5 pixel long lines "
+        + "separated by 5 pixels of whiespace. Only digits and whitespace are allowed (e.g., '12 34 56')",
+    )
+    circle_dash_offset = models.CharField(
+        # See Mozilla docs: https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Attribute/stroke-dashoffset#example.
+        max_length=32,
+        null=True,
+        blank=True,
+        help_text="Offset where the dash pattern starts along the outline path.",
     )
 
     # TODO: Add _hover style and _active style recursive foreign keys.
